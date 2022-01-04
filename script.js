de7b723124f4e15ae87365d9d00602d629614133
aziz <ahmed613@umn.edu>
2022-01-03 23:49:14 -0600

    DOM/html

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#seach-input');
 const searchList = document.querySelector('#search-list');
 let searchedCitiesArray=[]
+const currentWeather = document.querySelector('#current');
+const forecast = document.querySelector('#forecast')
