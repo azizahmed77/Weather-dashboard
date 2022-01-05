cd331f7a1fe300ef41038f2f1330bd94434a2e3d
aziz <ahmed613@umn.edu>
2022-01-04 23:49:31 -0600

    start getweather func

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const searchList = document.querySelector('#search-list');
 let searchedCitiesArray=[]
 const currentWeather = document.querySelector('#current');
 const forecast = document.querySelector('#forecast')
+
+const getWeather = async function(location){
+
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2c46a57714cfaf2673339b7b2f255993`
+    )
+    const data = await response.json();
+}
